25f76c535cb036c9a912c20fee1e41e482c8602a
Markus Sanin <markus.sanin@zeppelin-group.com>
2024-09-17 19:19:37 +0200

    fix test

--- ember-can/src/initializers/setup-ember-can.js
+++ ember-can/src/initializers/setup-ember-can.js
@@ -1,10 +1,11 @@
 ﻿import Resolver from 'ember-resolver';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 class EmberCanResolver extends Resolver {
   pluralizedTypes = {
     ...this.pluralizedTypes,
     ability: 'abilities',
-  }
+  };
 }
 
 export function initialize() {}
